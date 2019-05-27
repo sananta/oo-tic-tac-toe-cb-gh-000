35523278ba3e75def68c683239673bfbc0b19670
Varshini Ananta <sananta@ucdavis.edu>
2019-05-27 08:27:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,5 +110,16 @@ class TicTacToe
     end
   end
 
+  def play()
+    while !over?()
+      turn()
+    end
+
+    if won?()
+      puts "Congratulations #{winner()}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
 
 end
